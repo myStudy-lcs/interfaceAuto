368ef348c9a51898d59c739d782ea4b7c7e1e39e
Alan <lcs247735486@163.com>
2019-01-19 00:43:36 +0800

    testNg的基本注解

--- testNgDemo1/src/main/java/com/alipay/BasicAnnotation.java
+++ testNgDemo1/src/main/java/com/alipay/BasicAnnotation.java
@@ -1,8 +1,6 @@
 package com.alipay;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicAnnotation {
     @Test
@@ -13,6 +11,7 @@ public class BasicAnnotation {
     public void testCase2(){
         System.out.println("第2条测试用例");
     }
+
     @BeforeMethod
     public void beforeMethod(){
         System.out.println("~方法运行之前执行");
@@ -20,5 +19,23 @@ public class BasicAnnotation {
     @AfterMethod
     public void afterMethod(){
         System.out.println("方法运行之后执行~");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("~~类运行之前执行");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("类运行之后执行~~");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("~~BeforeSuite测试套件");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("AfterSuite测试套件~~");
     }
 }
