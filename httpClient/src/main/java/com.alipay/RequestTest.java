423172dc081efd3587bf0b0e1cade1fffceeb155
format2018 <lcs247735486@163.com>
2019-03-20 20:16:45 +0800

    HTTP的简单请求-接口自动化基础应用

---
+++ httpClient/src/main/java/com.alipay/RequestTest.java
@@ -0,0 +1,87 @@
+package com.alipay;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author ：web-lcs509306
+ * @Date ：2019/3/20 - 17:19
+ * 获取一个 get/psot 请求地址的返回数据
+ */
+public class RequestTest {
+    @Test
+    public void getTest() throws IOException {
+        /**
+         *@Description  ：发送一个get请求
+         *@Author       ：wb-lcs509306
+         *@Date         ：2019/3/20 - 17:21
+         *@Param        ：[args]
+         *@return       ：void
+         */
+        // 请求URL地址
+        String url = "http://mbd.baidu.com/newspage/data/landingsuper";
+        // 1 get请求
+        url += "?context=%7B%22nid%22%3A%22news_9106617718834593716%22%7D&n_type=0&p_from=1";
+        // 2 post请求
+
+
+        // 封装一个get请求方式
+        HttpGet get = new HttpGet(url);
+
+        // 发送请求的一个客户端
+//        HttpClient httpClient = new DefaultHttpClient();
+        HttpClient httpClient = HttpClients.createDefault();
+        // 执行请求,得到response响应
+        HttpResponse response = httpClient.execute(get);
+
+        // 提取响应返回的数据
+        String result = EntityUtils.toString(response.getEntity(), "utf-8");
+        System.out.println(result);
+    }
+
+    @Test
+    public void postTest() throws IOException {
+        /**
+         *@Description  ：发送一个post请求
+         *@Author       ：wb-lcs509306
+         *@Date         ：2019/3/20 - 17:21
+         *@Param        ：[args]
+         *@return       ：void
+         */
+
+        // 请求URL地址
+        String url = "http://bkmagicprod.sit.mayibank.net/home/bkcloudfundsTrade/openBalancePay";
+        // 封装一个post请求方式
+        HttpPost post = new HttpPost(url);
+
+        // post请求 请求参数封装在请求体中(没有参数可以不写)
+        List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+        parameters.add(new BasicNameValuePair("context","%7B%22nid%22%3A%22news_9106617718834593716%22%7D"));
+        parameters.add(new BasicNameValuePair("n_type","0"));
+        parameters.add(new BasicNameValuePair("p_from","1"));
+        // 设置参数的请求体
+//        post.setEntity(new UrlEncodedFormEntity(parameters));
+//        url += "?context=%7B%22nid%22%3A%22news_9106617718834593716%22%7D&n_type=0&p_from=1";
+
+        // 发送请求的一个客户端
+//        HttpClient httpClient = new DefaultHttpClient();
+        HttpClient httpClient = HttpClients.createDefault();
+        // 执行请求,得到response响应
+        HttpResponse response = httpClient.execute(post);
+
+        // 提取响应返回的数据
+        String result = EntityUtils.toString(response.getEntity(), "utf-8");
+        System.out.println(result);
+    }
+}
