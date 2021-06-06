8ba33a1b19a9ac742fe8e8700a408f72a2404d19
lcs <lcs247735486@163.com>
2021-06-06 21:17:43 +0800

    开始java自动化学习

---
+++ javaAutomation/src/main/java/com/lcs/java/FirstDemo.java
@@ -0,0 +1,12 @@
+package com.lcs.java;
+
+/**
+ * @Author: Alan
+ * @Date: 2021/6/6 21:16
+ * @Email: lcs247735486@163.com
+ **/
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
